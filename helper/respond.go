692e14324d9061165485cf342e11053c60abdd28
whoophy <arifkurniawan123160068@gmail.com>
2022-11-15 07:23:45 +0700

    add response template

---
+++ helper/respond.go
@@ -0,0 +1,15 @@
+package helper
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func respondwithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+	fmt.Println(payload)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
